555844d1a3eccc587abb7e3f54789d0ce77cc039
EthanRule <ethan2002@live.com>
2023-02-25 11:31:14 -0800

    first push

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# C-SANDBOX
+# C/C++-SANDBOX
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+    std::cout << "Hello World\n"; // endl flushes every iteration, while \n flushes once after everything is ready to print
+    }
+    return 0;
+}
